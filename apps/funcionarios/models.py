f200ddeb6dd08d2020a4c708c8f4d631c214b645
raffaell95 <raffaell95@live.com>
2021-03-10 01:18:15 -0300

    cruds

---
+++ apps/funcionarios/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from ..departamentos.models import Departamento
+from ..empresas.models import Empresa
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+    
+
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
+
+    def __str__(self):
+        return self.nome
